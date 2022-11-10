ca22048db876a0b3fb154fe1b31cf5aa40300057
EUNSH1 <tkdgus968@naver.com>
2022-11-10 17:42:33 +0900

    feat : ThorAuction basic frame

---
+++ src/main/java/com/goodee/finalproject/ThorAuctionApplication.java
@@ -0,0 +1,13 @@
+package com.goodee.finalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThorAuctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThorAuctionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goodee/finalproject/ThorAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodee.finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThorAuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
