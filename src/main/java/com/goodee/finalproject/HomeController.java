94c7d12716142d19a696a51c1d1b59fda08a82b5
kjg0807 <kjg08070@naver.com>
2022-11-14 17:46:47 +0900

    1

---
+++ src/main/java/com/goodee/finalproject/HomeController.java
@@ -0,0 +1,17 @@
+package com.goodee.finalproject;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+public class HomeController
+{
+	@GetMapping("/")
+	public String home() throws Exception
+	{
+		return "index";
+	}
+}
