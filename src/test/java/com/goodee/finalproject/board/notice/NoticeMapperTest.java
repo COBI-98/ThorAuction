5419f373db3bbf13349a543d4d31affab97f8f1c
EUNSH1 <tkdgus968@naver.com>
2022-11-17 19:08:43 +0900

    test : 게시판 test 데이터 입력

---
+++ src/test/java/com/goodee/finalproject/board/notice/NoticeMapperTest.java
@@ -0,0 +1,30 @@
+package com.goodee.finalproject.board.notice;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticeMapperTest {
+
+	@Autowired
+	private NoticeMapper noticeMapper;
+	
+	@Test
+	void test3() throws Exception {
+		NoticeVO noticeVO = new NoticeVO();
+		int result = 0;
+		for(int i = 10; i<30;i++) {
+
+			noticeVO.setTitle("Title"+i);
+			noticeVO.setWriter("COBI");
+			noticeVO.setContents("Contents"+i);
+			result = noticeMapper.setNoticeAdd(noticeVO);	
+			
+		}
+		assertNotEquals(0, result);
+	}
+
+}
