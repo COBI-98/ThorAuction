4ff972b92cfea07f21dc0385f483678ce480b121
Gumaaa <skc06040@naver.com>
2022-11-17 17:51:12 +0900

    member / 회원정보 수정 작업 중

--- src/main/java/com/goodee/finalproject/mypage/MypageService.java
+++ src/main/java/com/goodee/finalproject/mypage/MypageService.java
@@ -1,5 +1,7 @@
 package com.goodee.finalproject.mypage;
 
+import javax.servlet.ServletContext;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
