c843df1c5e3f1e31208d8c4f80bc9847b9c1ca8a
Gumaaa <skc06040@naver.com>
2022-11-18 12:10:30 +0900

    member / 아이디 중복확인 작업 중

--- src/main/java/com/goodee/finalproject/member/MemberMapper.java
+++ src/main/java/com/goodee/finalproject/member/MemberMapper.java
@@ -5,6 +5,9 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface MemberMapper {
 	
+	// 아이디 중복확인
+	public MemberVO getIdCheck(String id) throws Exception;
+	
 	// 로그인
 	public MemberVO getLogin(MemberVO memberVO) throws Exception;
 	
