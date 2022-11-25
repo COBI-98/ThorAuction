dae94d82b610fdcd0da668efe8f0f96017516d0a
Hongsugyeong <olivia258@naver.com>
2022-11-25 13:49:38 +0900

    최고가, id 저장 배열 생성

--- src/main/java/com/goodee/finalproject/socket/ChatController.java
+++ src/main/java/com/goodee/finalproject/socket/ChatController.java
@@ -1,7 +1,10 @@
 package com.goodee.finalproject.socket;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -10,11 +13,20 @@ public class ChatController {
 	WebSocketChat webSocketChat = new WebSocketChat();
 		
 	@RequestMapping("/chat")
-	public ModelAndView chat() {
+	public ModelAndView chat(HttpSession session) {
 		ModelAndView mv = new ModelAndView();
+		mv.addObject("value",webSocketChat.getValue());
+		System.out.println("value : "+webSocketChat.getValue());
 		mv.addObject("count",webSocketChat.getCount());
 		mv.setViewName("chat");
 		return mv;
 	}
 	
+//	@RequestMapping(value="/chat", method=RequestMethod.POST)
+//	public ModelAndView chat(int rank) {
+//		value = rank;
+//		System.out.println(value);
+//		return null;
+//	}
+
 }
