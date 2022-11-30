72186359de3ef292597a019e4ff756c2f7103147
kjg0807 <kjg08070@naver.com>
2022-11-30 09:14:45 +0900

    카카오 중복체크 미완, selectBox swal완료

--- src/main/resources/static/js/admin/kakaoLoginCheck.js
+++ src/main/resources/static/js/admin/kakaoLoginCheck.js
@@ -3,47 +3,35 @@
 // 어느 JSP에서 해야 할지
 
 function idcheck() {
-    // let kNickName = $("#kaName").val();
-    let kNickName = $(".kNickName").val();
+    swal({
+        title: "카카오 로그인",
+        icon: "info",
+    }).then(function () {
+        location = '/oauth2/authorization/kakao';
+    });
+    
+    // let kNickName = $(".kNickName").val();
 
-    console.log(kNickName);
+    // console.log(kNickName);
 
-    $.ajax({
-        url: '/socialMember/IdCheck',
-        type: "POST",
-        data: { kaNickName: kNickName },
-        traditional: true,
-        success: function (rs) {
-            console.log(rs);
-            swal({
-                title: "카카오 로그인",
-                icon: "info",
-            }).then(function () {
-                if (rs == 0) {
-                    swal({
-                        title: "카카오 회원가입",
-                        icon: "info",
-                    }).then(function () {
-                        location = '/oauth2/authorization/kakao';
-                    })
-                    // location = '/kakaoLogin';
-                }
-                else {
-                    swal({
-                        title: "연결이 유효함",
-                        text: '카카오로 로그인합니다.',
-                        icon: "success",
-                    }).then(function () {
-                        location = '/oauth2/authorization/kakao';
-                    })
-                    // location = '/';
-                }
-            });
-        },
-        error: function (textStatus, errorThrown) {
-            alert("서버 요청 실패");
-            console.log(textStatus);
-            console.log(errorThrown);
-        }
-    })
+    // $.ajax({
+    //     url: '/socialMember/IdCheck',
+    //     type: "POST",
+    //     data: { kaNickName: kNickName },
+    //     traditional: true,
+    //     success: function (rs) {
+    //         console.log(rs);
+    //         swal({
+    //             title: "카카오 로그인",
+    //             icon: "info",
+    //         }).then(function () {
+    //             location = '/oauth2/authorization/kakao';
+    //         });
+    //     },
+    //     error: function (textStatus, errorThrown) {
+    //         alert("서버 요청 실패");
+    //         console.log(textStatus);
+    //         console.log(errorThrown);
+    //     }
+    // })
 }
