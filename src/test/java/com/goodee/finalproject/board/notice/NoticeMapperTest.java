803e5d4d3f69b20ff8c82bf33d7c86d077ddbc10
EUNSH1 <tkdgus968@naver.com>
2022-12-01 09:14:00 +0900

    test : 공지게시판 테스트 데이터 추가

--- src/test/java/com/goodee/finalproject/board/notice/NoticeMapperTest.java
+++ src/test/java/com/goodee/finalproject/board/notice/NoticeMapperTest.java
@@ -12,15 +12,16 @@ class NoticeMapperTest {
 	@Autowired
 	private NoticeMapper noticeMapper;
 	
-	//@Test
+	@Test
 	void test3() throws Exception {
 		NoticeVO noticeVO = new NoticeVO();
 		int result = 0;
-		for(int i = 10; i<30;i++) {
+		for(int i = 0; i<30;i++) {
 
 			noticeVO.setTitle("Title"+i);
 			noticeVO.setWriter("COBI");
 			noticeVO.setContents("Contents"+i);
+			noticeVO.setImportCheck(false);
 			result = noticeMapper.setNoticeAdd(noticeVO);	
 			
 		}
