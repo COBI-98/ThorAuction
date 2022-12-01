09a0bb23f74c6b11dda1919ad7e2b36d58bc42ec
EUNSH1 <tkdgus968@naver.com>
2022-12-01 10:08:30 +0900

    feat : 헤더 푸터 추가

--- src/main/java/com/goodee/finalproject/product/ProductController.java
+++ src/main/java/com/goodee/finalproject/product/ProductController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
@@ -39,6 +40,11 @@ public class ProductController {
 		mv.setViewName("redirect:./list");
 		redirectAttributes.addAttribute("result", result);
 		return mv;
+	}
+	
+	@GetMapping("approval")
+	public void getApprovalProduct(ProductVO productVO) throws Exception{
+		
 	}
 	
 }
