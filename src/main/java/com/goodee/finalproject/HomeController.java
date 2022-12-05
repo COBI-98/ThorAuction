dc93e85c9eaca8fe560ab54c6b9daba0ee390480
kjg0807 <kjg08070@naver.com>
2022-12-05 17:51:33 +0900

    멤버 벤 미완

--- src/main/java/com/goodee/finalproject/HomeController.java
+++ src/main/java/com/goodee/finalproject/HomeController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,23 +35,21 @@ public class HomeController
 	private String logout_uri;
 
 	@GetMapping("/")
-	public ModelAndView home(MemberVO memberVO, HttpSession session, KakaoVO kakaoVO,
+	public ModelAndView home(MemberVO memberVOo, HttpSession session, KakaoVO kakaoVO,
 			@RequestParam(value = "role", required = false) String role, HttpServletRequest request, HttpServletResponse response)
 			throws Exception
 	{
 		log.info("===== home controller =====");
-		log.info("param Role: {}", role);
+		// log.info("param Role: {}", role);
 
 		ModelAndView modelAndView = new ModelAndView();
 
 		List<KakaoVO> kakaoVOs = adminService.getKakaoTotal(kakaoVO);
+		List<MemberVO> memberVOs = adminService.getMemberTotal(memberVOo);
 
-//		List<MemberVO> memberVOs = adminService.getMemberTotal(memberVO);
-//		modelAndView.addObject("member", memberVOs);
-
+		modelAndView.addObject("member", memberVOs);
 		modelAndView.addObject("kakaoRole", kakaoVOs);
-		modelAndView.setViewName("index");
-
+		// modelAndView.setViewName("index");
 		modelAndView.addObject("ka", session.getAttribute("kakaoInfo"));
 		modelAndView.setViewName("index");
 
