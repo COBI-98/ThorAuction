a52a4986631a8b10797e5c02ebb27993aeb82c10
EUNSH1 <tkdgus968@naver.com>
2022-12-13 18:05:53 +0900

    feat : 관심있는 상품 VO 추가

---
+++ src/main/java/com/goodee/finalproject/product/LikeSaleProductVO.java
@@ -0,0 +1,11 @@
+package com.goodee.finalproject.product;
+
+import lombok.Data;
+
+@Data
+public class LikeSaleProductVO {
+
+	private Long likeNum;
+	private Long productId;
+	private String id;
+}
