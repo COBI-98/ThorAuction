b7d04bedf21501ea53f05f617ca3a2550e585696
Gumaaa <skc06040@naver.com>
2022-12-14 09:33:14 +0900

    mypage / 엔터 코드 추가

--- src/main/resources/static/js/mypage/checkpw.js
+++ src/main/resources/static/js/mypage/checkpw.js
@@ -37,3 +37,44 @@ $('#checkpw_btn').click(function() {
 		})
 	}
 });
+
+// 비밀번호 란에서 엔터 누를 시
+$("#pw").keyup(function(key) {
+    if (key.keyCode == 13) {
+		const id = $('#id').val();
+	const checkPassword = $('#pw').val();
+	
+	if(!checkPassword || checkPassword.trim() === ""){
+		Swal.fire({
+			icon: 'warning',
+			title: '잠시만요!',
+			text: '비밀번호를 입력하세요',
+		})
+	} else{
+		$.ajax({
+			type: 'POST',
+			url: '/mypage/checkpw',
+			data: {'checkPassword': checkPassword},
+			datatype: "text"
+		}).done(function(result){
+			console.log(result);
+			if(result){
+				console.log("비밀번호 일치");
+				location.href="/mypage/update?id=" + id;
+			} else if(!result){
+				console.log("비밀번호 틀림");
+				// 비밀번호가 일치하지 않으면
+				Swal.fire({
+					icon: 'error',
+					title: '잠시만요!',
+					text: '비밀번호가 일치하지 않습니다',
+				}).then(function() {
+				window.location.reload();
+			})
+			}
+		}).fail(function(error){
+			alert(JSON.stringify(error));
+		})
+	}
+	}
+});
